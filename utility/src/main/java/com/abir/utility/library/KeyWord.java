d5fd9eadcc7fb38c4679dbdfbb93ae2133c02519
Muhaiminur Rahman <muhaiminurabir@gmail.com>
2022-01-06 11:58:15 +0600

    version 1.0.1

---
+++ utility/src/main/java/com/abir/utility/library/KeyWord.java
@@ -0,0 +1,12 @@
+package com.abir.utility.library;
+
+public class KeyWord {
+
+    //YES NO
+    public static String YES = "yes";
+    public static String NO = "no";
+
+    public static String SCREEN_WIDTH = "width";
+    public static String SCREEN_HEIGHT = "height";
+    public static String SCREEN_DENSITY = "density";
+}
